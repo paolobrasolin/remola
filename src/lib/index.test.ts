51283bec6826fda2f151eae3721f0f1b79153e3d
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-02-09 17:01:57 +0100

    feat: ts code for signatures and composition

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -1,5 +1,69 @@
-import { greet } from "./index";
+import { compose, indicesToDomain, sigFromIdxs } from "./index";
 
-test("greeting", () => {
-  expect(greet("test")).toBe("Hello test!");
+(
+  [
+    [1, [], 0b0],
+    [1, [1], 0b1],
+    [1, [1, 1], 0b11],
+    [1, [1, 1, 1], 0b111],
+    [2, [], 0b00],
+    [2, [1], 0b01],
+    [2, [2], 0b10],
+    [2, [1, 1], 0b0101],
+    [2, [1, 2], 0b1001],
+    [2, [2, 1], 0b0110],
+    [2, [2, 2], 0b1010],
+  ] as [number, number[], number][]
+).forEach(([bits, idcs, dom]) => {
+  test(`${bits} bits: ${JSON.stringify(idcs)} => ${dom}`, () => {
+    expect(indicesToDomain(idcs, bits)).toBe(dom);
+  });
+});
+
+test("empty", () => {
+  expect(sigFromIdxs([], [], 2)).toStrictEqual({
+    arity: 0,
+    domain: 0,
+    coarity: 0,
+    codomain: 0,
+  });
+});
+
+test("nonempty", () => {
+  expect(sigFromIdxs([2, 3], [1, 2, 2, 1], 2)).toStrictEqual({
+    arity: 2,
+    domain: 0b1110,
+    coarity: 4,
+    codomain: 0b01101001,
+  });
+});
+
+const PAR = {
+  lRoot: sigFromIdxs([], [1], 2),
+  lNest: sigFromIdxs([1], [2, 1], 2),
+  rNest: sigFromIdxs([2, 1], [1], 2),
+  rRoot: sigFromIdxs([1], [], 2),
+};
+
+test("compose", () => {
+  expect(compose(PAR.lRoot, PAR.lNest)).toStrictEqual([
+    [0, sigFromIdxs([], [2, 1], 2)],
+  ]);
+});
+
+test("compose moar", () => {
+  expect(
+    compose(
+      {
+        arity: 0,
+        domain: 0b0,
+        coarity: 4,
+        codomain: 0b01101001,
+      },
+      PAR.lNest
+    )
+  ).toStrictEqual([
+    [0, sigFromIdxs([], [...[2, 1], 2, 2, 1], 2)],
+    [3, sigFromIdxs([], [1, 2, 2, ...[2, 1]], 2)],
+  ]);
 });
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,51 @@
 export const greet = (name: string): string => `Hello ${name}!`;
+
+interface Signature {
+  arity: number;
+  domain: number;
+  coarity: number;
+  codomain: number;
+}
+
+export type Diagram = Signature;
+export type Generator = Signature;
+
+export function indicesToDomain(indices: number[], bits: number): number {
+  return indices.reduce((p, c, i) => p + (c << (i * bits)), 0);
+}
+
+export function sigFromIdxs(
+  domSig: number[],
+  codSig: number[],
+  bits: number
+): Signature {
+  return {
+    arity: domSig.length,
+    domain: indicesToDomain(domSig, bits),
+    coarity: codSig.length,
+    codomain: indicesToDomain(codSig, bits),
+  };
+}
+
+export function compose(dia: Diagram, gen: Generator): [number, Diagram][] {
+  const results: [number, Diagram][] = [];
+  for (let offset = 0; offset <= dia.coarity - gen.arity + 1; offset++) {
+    const l = gen.arity * 2;
+    const o = offset * 2;
+    const body = (dia.codomain & (2 ** (l + o) - 1)) >> o;
+    if (body == gen.domain) {
+      const head = dia.codomain >> (l + o);
+      const tail = dia.codomain & (2 ** o - 1);
+      const new_sig =
+        (((head << (gen.coarity * 2)) + gen.codomain) << (offset * 2)) + tail;
+      const signature: Signature = {
+        arity: dia.arity,
+        domain: dia.domain,
+        coarity: dia.coarity - gen.arity + gen.coarity,
+        codomain: new_sig,
+      };
+      results.push([offset, signature]);
+    }
+  }
+  return results;
+}
