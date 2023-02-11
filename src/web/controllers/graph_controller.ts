e9d46befdd522fe2f75eded29c8298e1e054e148
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-02-11 22:20:28 +0100

    feat: sketch GraphController

--- src/web/controllers/graph_controller.ts
+++ src/web/controllers/graph_controller.ts
@@ -1,83 +1,95 @@
-import cytoscape from "cytoscape";
-import "./index.css";
+import cytoscape, { Stylesheet } from "cytoscape";
 import { sigFromIdcs, Sig, explore } from "../../lib/index";
 
-const bits = 2n;
-const balParLangGenerators = [
-  sigFromIdcs([], [0b01n], bits),
-  sigFromIdcs([0b01n], [0b10n, 0b01n], bits),
-  sigFromIdcs([0b10n, 0b01n], [0b01n], bits),
-  sigFromIdcs([0b01n], [], bits),
+import { Controller } from "@hotwired/stimulus";
+
+const STYLE: Stylesheet[] = [
+  {
+    selector: "node",
+    style: {
+      "background-color": "goldenrod",
+      width: "label",
+      height: "label",
+      "padding-top": "4",
+      "padding-bottom": "4",
+      "padding-left": "4",
+      "padding-right": "4",
+      shape: "round-rectangle",
+    },
+  },
+  {
+    selector: "node[label]",
+    style: {
+      label: "data(label)",
+      color: "white",
+      "font-size": "16",
+      "text-halign": "center",
+      "text-valign": "center",
+    },
+  },
+  {
+    selector: "edge",
+    style: {
+      "curve-style": "bezier",
+      "target-arrow-shape": "triangle",
+      width: 1.6,
+    },
+  },
+  {
+    selector: "edge[label]",
+    style: {
+      label: "data(label)",
+      "font-size": "12",
+      "text-background-color": "white",
+      "text-background-opacity": 1,
+      "text-background-padding": "2px",
+      "text-events": "yes",
+    },
+  },
 ];
-const start: Sig[] = [sigFromIdcs([], [], bits).cod];
-const store = new Map<bigint, Set<bigint>>();
-explore(start, store, 6n, balParLangGenerators, bits);
 
-const cy = cytoscape({
-  container: document.getElementById("app"),
-  // elements: [{ data: { id: "a" } }, { data: { id: "b" } }],
-  style: [
-    {
-      selector: "node",
-      style: {
-        "background-color": "goldenrod",
-        width: "label",
-        height: "label",
-        "padding-top": "4",
-        "padding-bottom": "4",
-        "padding-left": "4",
-        "padding-right": "4",
-        shape: "round-rectangle",
-      },
-    },
-    {
-      selector: "node[label]",
-      style: {
-        label: "data(label)",
-        color: "white",
-        "font-size": "16",
-        "text-halign": "center",
-        "text-valign": "center",
-      },
-    },
-    {
-      selector: "edge",
-      style: {
-        "curve-style": "bezier",
-        "target-arrow-shape": "triangle",
-        width: 1.6,
-      },
-    },
-    {
-      selector: "edge[label]",
-      style: {
-        label: "data(label)",
-        "font-size": "12",
-        "text-background-color": "white",
-        "text-background-opacity": 1,
-        "text-background-padding": "2px",
-        "text-events": "yes",
-      },
-    },
-  ],
-});
+export default class extends Controller {
+  static targets = ["container"];
+  declare readonly containerTarget: HTMLDivElement;
 
-for (const k of store.keys()) {
-  const data = { id: k.toString(36), label: k.toString(2) };
-  cy.add({ data: data });
-}
+  connect() {
+    const bits = 2n;
+    const balParLangGenerators = [
+      sigFromIdcs([], [0b01n], bits),
+      sigFromIdcs([0b01n], [0b10n, 0b01n], bits),
+      sigFromIdcs([0b10n, 0b01n], [0b01n], bits),
+      sigFromIdcs([0b01n], [], bits),
+    ];
+    const start: Sig[] = [sigFromIdcs([], [], bits).cod];
+    const store = new Map<bigint, Set<bigint>>();
+    explore(start, store, 6n, balParLangGenerators, bits);
 
-for (const [source, targets] of store.entries()) {
-  for (const target of targets) {
-    if (!store.has(target)) cy.add({ data: { id: target.toString(36) } });
+    const cy = cytoscape({
+      container: this.containerTarget,
+      // elements: [{ data: { id: "a" } }, { data: { id: "b" } }],
+      style: STYLE,
+    });
 
-    const data = {
-      source: source.toString(36),
-      target: target.toString(36),
-      label: "01/0",
-    };
-    cy.add({ data });
+    for (const k of store.keys()) {
+      const data = { id: k.toString(36), label: k.toString(2) };
+      cy.add({ data: data });
+    }
+
+    for (const [source, targets] of store.entries()) {
+      for (const target of targets) {
+        if (!store.has(target)) cy.add({ data: { id: target.toString(36) } });
+
+        const data = {
+          source: source.toString(36),
+          target: target.toString(36),
+          label: "01/0",
+        };
+        cy.add({ data });
+      }
+    }
+
+    cy.layout({ name: "cose" }).run();
   }
-}
 
-cy.layout({ name: "cose" }).run();
+  disconnect() {}
+}
--- src/web/index.ts
+++ src/web/index.ts
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus";
 import TabController from "./controllers/tab_controller";
 import EditorController from "./controllers/editor_controller";
+import GraphController from "./controllers/graph_controller";
 import "./index.css";
 
 const application = Application.start();
 application.register("tab", TabController);
 application.register("editor", EditorController);
+application.register("graph", GraphController);
 application.debug = true;
