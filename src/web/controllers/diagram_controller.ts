1ea005028a10e319cceeca0897e294ff7391fd24
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-04-11 00:29:57 +0200

    feat: sketch diagram controller

--- src/web/controllers/diagram_controller.ts
+++ src/web/controllers/diagram_controller.ts
@@ -1,24 +1,35 @@
 import * as svgjs from "@svgdotjs/svg.js";
 import { drawDiagram } from "../draw";
 
-const w = 200;
-const h = 300;
+import { Controller } from "@hotwired/stimulus";
 
-const svg = svgjs
-  .SVG()
-  .addTo("body")
-  .size(w * 2, h);
+export default class extends Controller {
+  static targets = ["container"];
+  declare readonly containerTarget: HTMLDivElement;
 
-drawDiagram(
-  [
-    // Sig, Dom, Cod, Cosig, Offset
-    [0, 1, 6, 6, 0],
-    [6, 2, 1, 5, 2],
-    [5, 2, 1, 4, 1],
-    [4, 2, 1, 3, 1],
-    [3, 2, 1, 2, 1],
-    [2, 2, 0, 0, 0],
-  ],
-  [0, w, 0, h],
-  svg
-);
+  connect() {
+    const w = 200;
+    const h = 300;
+
+    const svg = svgjs
+      .SVG()
+      .addTo(this.containerTarget)
+      .size(w * 2, h);
+
+    drawDiagram(
+      [
+        // Sig, Dom, Cod, Cosig, Offset
+        [0, 1, 6, 6, 0],
+        [6, 2, 1, 5, 2],
+        [5, 2, 1, 4, 1],
+        [4, 2, 1, 3, 1],
+        [3, 2, 1, 2, 1],
+        [2, 2, 0, 0, 0],
+      ],
+      [0, w, 0, h],
+      svg
+    );
+  }
+
+  disconnect() {}
+}
--- src/web/index.ts
+++ src/web/index.ts
@@ -2,6 +2,7 @@ import { Application } from "@hotwired/stimulus";
 import TabController from "./controllers/tab_controller";
 import EditorController from "./controllers/editor_controller";
 import GraphController from "./controllers/graph_controller";
+import DiagramController from "./controllers/diagram_controller";
 import "./monaco_environment";
 import "./index.css";
 
@@ -9,4 +10,5 @@ const application = Application.start();
 application.register("tab", TabController);
 application.register("editor", EditorController);
 application.register("graph", GraphController);
+application.register("diagram", DiagramController);
 application.debug = true;
