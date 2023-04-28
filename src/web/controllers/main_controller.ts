72f34735f250a47453eb2cc50831b3bb0f1e9298
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-04-28 19:29:19 +0200

    feat: minimal exploration dump

--- src/web/controllers/main_controller.ts
+++ src/web/controllers/main_controller.ts
@@ -1,7 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
+import {
+  Composable,
+  HumanGrammar,
+  humanToMachineGrammar,
+  indicesToGenerator,
+} from "../../lib/encoding";
+import { explore } from "../../lib/generation";
 
 export default class extends Controller {
-  logEvent({ type, detail }: CustomEvent) {
-    console.log(type, detail);
+  logEvent({
+    type,
+    detail: { grammar },
+  }: CustomEvent<{ grammar: HumanGrammar }>) {
+    console.log(type, grammar);
+    const machineGrammar = humanToMachineGrammar(grammar);
+
+    const seed: Composable[] = [
+      indicesToGenerator([], [], machineGrammar.bits),
+    ];
+    const store = new Map<bigint, Set<bigint>>();
+
+    const generators = [...machineGrammar.generators.values()];
+    explore(seed, store, 4n, generators, machineGrammar.bits);
+
+    console.log(store);
   }
 }
