ed4b52311856b3f86a3a36956b43ae602a70cfa8
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-05-07 11:29:19 +0200

    feat: test encoding of all grammars in arXiv:2207.00526

--- src/lib/encoding.test.ts
+++ src/lib/encoding.test.ts
@@ -64,6 +64,70 @@ describe("humanToMachineGrammar", () => {
     };
   }>([
     {
+      grammarName: "Empty",
+      humanGrammar: {},
+      flatMachineGrammar: {
+        bits: 0n,
+        alphabet: [],
+        generators: [],
+      },
+    },
+    {
+      grammarName: "Dust",
+      humanGrammar: {
+        "*": { dom: [], cod: [] },
+      },
+      flatMachineGrammar: {
+        bits: 0n,
+        alphabet: [],
+        generators: [[Symbol.for("*"), 0n, 0n, 0n, 0n]],
+      },
+    },
+    {
+      grammarName: "Loop",
+      humanGrammar: {
+        "*": { dom: ["X"], cod: ["X"] },
+      },
+      flatMachineGrammar: {
+        bits: 1n,
+        alphabet: [[Symbol.for("X"), 0b1n]],
+        generators: [[Symbol.for("*"), 1n, 0b1n, 1n, 0b1n]],
+      },
+    },
+    {
+      grammarName: "Cycle",
+      humanGrammar: {
+        f: { dom: ["A"], cod: ["B"] },
+        b: { dom: ["B"], cod: ["A"] },
+      },
+      flatMachineGrammar: {
+        bits: 2n,
+        alphabet: [
+          [Symbol.for("A"), 0b01n],
+          [Symbol.for("B"), 0b10n],
+        ],
+        generators: [
+          [Symbol.for("f"), 1n, 0b01n, 1n, 0b10n],
+          [Symbol.for("b"), 1n, 0b10n, 1n, 0b01n],
+        ],
+      },
+    },
+    {
+      grammarName: "Bubbles",
+      humanGrammar: {
+        "<": { dom: ["A"], cod: ["A", "A"] },
+        ">": { dom: ["A", "A"], cod: ["A"] },
+      },
+      flatMachineGrammar: {
+        bits: 1n,
+        alphabet: [[Symbol.for("A"), 0b01n]],
+        generators: [
+          [Symbol.for("<"), 1n, 0b1n, 2n, 0b1_1n],
+          [Symbol.for(">"), 2n, 0b1_1n, 1n, 0b1n],
+        ],
+      },
+    },
+    {
       grammarName: "Balanced parentheses",
       humanGrammar: {
         "0": { dom: [], cod: ["A"] },
@@ -82,6 +146,54 @@ describe("humanToMachineGrammar", () => {
           [Symbol.for("("), 1n, 0b01n, 2n, 0b01_10n],
           [Symbol.for(")"), 2n, 0b01_10n, 1n, 0b01n],
           [Symbol.for("1"), 1n, 0b01n, 0n, 0b0n],
+        ],
+      },
+    },
+    {
+      grammarName: "Brick walls",
+      humanGrammar: {
+        "0": { dom: [], cod: ["H", "V"] },
+        "*": { dom: ["H", "V"], cod: ["V", "H"] },
+        "1": { dom: ["V", "H"], cod: [] },
+      },
+      flatMachineGrammar: {
+        bits: 2n,
+        alphabet: [
+          [Symbol.for("H"), 0b01n],
+          [Symbol.for("V"), 0b10n],
+        ],
+        generators: [
+          [Symbol.for("0"), 0n, 0b0n, 2n, 0b10_01n],
+          [Symbol.for("*"), 2n, 0b10_01n, 2n, 0b01_10n],
+          [Symbol.for("1"), 2n, 0b01_10n, 0n, 0b0n],
+        ],
+      },
+    },
+    {
+      grammarName: "Sierpiński gasket",
+      humanGrammar: {
+        X0: { dom: [], cod: ["H1", "V1"] },
+        B0: { dom: ["H1", "V1"], cod: ["V0", "H0"] },
+        B1: { dom: ["H0", "V0"], cod: ["V0", "H0"] },
+        Y0: { dom: ["H0", "V1"], cod: ["V1", "H1"] },
+        Y1: { dom: ["H1", "V0"], cod: ["V1", "H1"] },
+        X1: { dom: ["V1", "H1"], cod: [] },
+      },
+      flatMachineGrammar: {
+        bits: 3n,
+        alphabet: [
+          [Symbol.for("H1"), 0b001n],
+          [Symbol.for("V1"), 0b010n],
+          [Symbol.for("V0"), 0b011n],
+          [Symbol.for("H0"), 0b100n],
+        ],
+        generators: [
+          [Symbol.for("X0"), 0n, 0n, 2n, 0b010_001n],
+          [Symbol.for("B0"), 2n, 0b010_001n, 2n, 0b100_011n],
+          [Symbol.for("B1"), 2n, 0b011_100n, 2n, 0b100_011n],
+          [Symbol.for("Y0"), 2n, 0b010_100n, 2n, 0b001_010n],
+          [Symbol.for("Y1"), 2n, 0b011_001n, 2n, 0b001_010n],
+          [Symbol.for("X1"), 2n, 0b001_010n, 0n, 0b0n],
         ],
       },
     },
